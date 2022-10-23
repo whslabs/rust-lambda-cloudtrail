4ecc06c38d82c509d1002b6ab7c0bcfaebe90d66
whs <hswongac@gmail.com>
2022-10-23 04:16:16 -0400

    Add flake support

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs = {
+    fenix.url = "github:nix-community/fenix";
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, fenix, flake-utils, naersk, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+
+        toolchain = with fenix.packages.${system};
+          combine [
+            minimal.cargo
+            minimal.rustc
+            targets.x86_64-unknown-linux-musl.latest.rust-std
+          ];
+
+        naersk' = naersk.lib.${system}.override {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+
+      in rec {
+        defaultPackage = naersk'.buildPackage {
+          CARGO_BUILD_TARGET = "x86_64-unknown-linux-musl";
+          doCheck = true;
+          nativeBuildInputs = with pkgs; [ pkgsStatic.stdenv.cc ];
+          src = ./.;
+        };
+      }
+    );
+}
